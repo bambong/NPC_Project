c78ce5ce49345c18fdb6100e45537fd7a88103d8
bambong <alsrbs2gh@gmail.com>
2022-11-29 01:21:11 +0900

    키워드 드래그 앤 드롭 구현 , 키워드 애니메이션 구현

---
+++ NpcProject/Assets/Developers/Bambong/Scripts/DragableController.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragableController : MonoBehaviour, IDragHandler, IEndDragHandler,IBeginDragHandler ,IPointerExitHandler ,IPointerEnterHandler
+{
+    private readonly float START_END_ANIM_TIME = 0.2f;
+    private readonly float FOCUSING_SCALE = 1.2f;
+    [SerializeField]
+    private RectTransform rectTransform;
+
+    private int prevSibilintIndex;
+    private Coroutine curAnimCoroutine;
+    private Vector3 startDragPoint;
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+
+        prevSibilintIndex = rectTransform.GetSiblingIndex();
+        rectTransform.SetAsLastSibling();
+        startDragPoint = rectTransform.anchoredPosition;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        rectTransform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        rectTransform.anchoredPosition = startDragPoint;  
+        rectTransform.SetSiblingIndex(prevSibilintIndex);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        ClearAnim();
+        curAnimCoroutine = StartCoroutine(ChangeScaleLerpAnim(new Vector3(FOCUSING_SCALE, FOCUSING_SCALE, FOCUSING_SCALE), START_END_ANIM_TIME));
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        ClearAnim();
+        curAnimCoroutine = StartCoroutine(ChangeScaleLerpAnim(Vector3.one, START_END_ANIM_TIME));  
+    }
+
+    public void ClearAnim() 
+    {
+        if(curAnimCoroutine != null) 
+        {
+            StopCoroutine(curAnimCoroutine);
+        }
+    }
+
+
+    public IEnumerator ChangeScaleLerpAnim(Vector3 desireScale, float time) 
+    {
+        float progress = 0f;
+        float revisionNum = 1 / time;
+        var startScale = rectTransform.localScale;
+
+        while (progress < 1) 
+        {
+            yield return null;
+            progress += Time.deltaTime * revisionNum;
+            rectTransform.localScale = Vector3.Lerp(startScale, desireScale, progress); 
+        }
+        rectTransform.localScale = desireScale;
+    }
+
+
+}
