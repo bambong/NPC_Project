fcbaa7c91b47066468796db727b065b45524e9db
ironbear <20192492@g.dongseo.ac.kr>
2023-07-12 20:51:21 +0900

    설정창 임시 생성

--- NpcProject/Assets/Developers/Ironbear/Scripts/Settings/SettingPanelController.cs
+++ NpcProject/Assets/Developers/Ironbear/Scripts/Settings/SettingPanelController.cs
@@ -6,6 +6,19 @@ public class SettingPanelController : MonoBehaviour
 {
     [SerializeField]
     private GameObject settingPanel;
+    [SerializeField]
+    private GameObject inputs;
+    [SerializeField]
+    private GameObject sounds;
+
+    private bool isInputs = false;
+    private bool isSounds = false;
+
+
+    private void Awake()
+    {
+        isInputs = true;
+    }
 
     public void OnSettingPanel()
     {
@@ -15,5 +28,27 @@ public class SettingPanelController : MonoBehaviour
     public void OffSettingPanel()
     {
         settingPanel.SetActive(false);
+    }
+
+    public void OnInputs()
+    {
+        if(isSounds)
+        {
+            sounds.SetActive(false);
+            isSounds = false;
+        }
+        inputs.SetActive(true);
+        isInputs = true;
+    }
+
+    public void OnSounds()
+    {
+        if(isInputs)
+        {
+            inputs.SetActive(false);
+            isInputs = false;
+        }
+        sounds.SetActive(true);
+        isSounds = true;
     }
 }
