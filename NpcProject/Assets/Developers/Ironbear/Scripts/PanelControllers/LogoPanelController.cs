c93f18f9eda3ac3a6db0f9815c41611b0ce8b791
ironbear <20192492@g.dongseo.ac.kr>
2023-07-27 22:42:04 +0900

    시작 화면 배경 이미지 변경

--- NpcProject/Assets/Developers/Ironbear/Scripts/PanelControllers/LogoPanelController.cs
+++ NpcProject/Assets/Developers/Ironbear/Scripts/PanelControllers/LogoPanelController.cs
@@ -1,10 +1,6 @@
 using UnityEngine;
 using DG.Tweening;
 using UnityEngine.UI;
-using System.Collections;
-using UnityEngine.Rendering;
-using UnityEngine.Rendering.Universal;
-using UnityEditor;
 
 public class LogoPanelController : UI_Base
 {
@@ -12,8 +8,6 @@ public class LogoPanelController : UI_Base
     private GameObject logos;
     [SerializeField]
     private GameObject btns;
-    [SerializeField]
-    private GameObject bar;
     [SerializeField]
     private GameObject title;
 
@@ -25,10 +19,24 @@ public class LogoPanelController : UI_Base
     [SerializeField]
     private GameObject logo;
 
+    [SerializeField]
+    private GameObject firstPanel;
+    [SerializeField]
+    private GameObject secondPanel;
+
     private Image rend;
 
-    private float moveDuration = 0.8f;
-    private float moveDelay = 0.1f;
+    //private float moveDuration = 0.8f;
+    //private float moveDelay = 0.1f;
+
+    [SerializeField]
+    private CanvasGroup firstPanelCanvas;
+    [SerializeField]
+    private CanvasGroup txtCanvasGroup;
+    private float fadeInDuration = 1f;
+
+    private float fadeOutDuration = 1f;
+    private float blinkInterval = 1f;
 
 
     public override void Init()
@@ -44,37 +52,73 @@ public class LogoPanelController : UI_Base
         rend = logo.GetComponent<Image>();
         rend.material = effectMat;
         rend.material.SetFloat("_AutoManualAnimation", 0);
+        firstPanel.SetActive(true);
+        secondPanel.SetActive(false);
 
-        logos.transform.localPosition = new Vector3(-1100, 0, 0);
-        bar.transform.localPosition = new Vector3(-1600, 160, 0);
-
-        for (int i = 0; i < btnsList.Length; i++)
-        {
-            GameObject btn = btnsList[i];
-            var pos = btn.transform.localPosition;
-            pos.x -= 400;
-            btn.transform.localPosition = pos; 
-        }
-        MoveObject();
+        Blink();
+        //logos.transform.localPosition = new Vector3(-1100, 0, 0);
+        //for (int i = 0; i < btnsList.Length; i++)
+        //{
+        //    GameObject btn = btnsList[i];
+        //    var pos = btn.transform.localPosition;
+        //    pos.x -= 400;
+        //    btn.transform.localPosition = pos; 
+        //}
+        //MoveObject();      
     }
 
-    private void MoveObject()
+    private void Update()
     {
-        logos.transform.DOLocalMoveX(logos.transform.localPosition.x + 1100, moveDuration).SetEase(Ease.OutQuad);
-        bar.transform.DOLocalMoveX(bar.transform.localPosition.x + 1150, moveDuration).SetEase(Ease.OutQuad);
-
-        for (int i = 0; i < btnsList.Length; i++)
+        if(Input.anyKeyDown)
         {
-            GameObject btn = btnsList[i];
-            float delay = i * moveDelay * 0.5f;
-
-            btn.transform.DOLocalMoveX(btn.transform.localPosition.x + 400, moveDuration).SetEase(Ease.OutQuad).SetDelay(delay);
+            FadePanelEffect();
         }
-
-        EnableButtonInteractable();
-  
-        Invoke("RestoreMaterial", moveDuration + 0.8f);
     }
+
+    private void FadePanelEffect()
+    {
+        firstPanelCanvas.DOFade(0f, fadeInDuration / 2)
+            .SetEase(Ease.InOutQuad)
+            .OnComplete(() =>
+            {
+                firstPanel.SetActive(false);
+                secondPanel.SetActive(true);
+            });
+    }
+
+    private void Blink()
+    {
+        txtCanvasGroup.DOFade(0f, fadeInDuration)
+            .SetEase(Ease.InOutQuad)
+            .OnComplete(() =>
+            {
+                txtCanvasGroup.DOFade(1f, fadeOutDuration)
+                    .SetEase(Ease.InOutQuad)
+                    .OnComplete(() =>
+                    {
+                        DOVirtual.DelayedCall(blinkInterval, Blink);
+                    });
+            });
+
+        Invoke("RestoreMaterial", 1.6f);
+    }
+
+    //private void MoveObject()
+    //{
+    //    logos.transform.DOLocalMoveX(logos.transform.localPosition.x + 1100, moveDuration).SetEase(Ease.OutQuad);
+
+    //    for (int i = 0; i < btnsList.Length; i++)
+    //    {
+    //        GameObject btn = btnsList[i];
+    //        float delay = i * moveDelay * 0.5f;
+
+    //        btn.transform.DOLocalMoveX(btn.transform.localPosition.x + 400, moveDuration).SetEase(Ease.OutQuad).SetDelay(delay);
+    //    }
+
+    //    EnableButtonInteractable();
+  
+    //    Invoke("RestoreMaterial", moveDuration + 0.8f);
+    //}
 
     private void RestoreMaterial()
     {
