6bb1ffefd56a46bb5e2cbfb66285dc864c15a2b1
이진우 <>
2023-08-07 14:51:07 +0900

    추가: 디버그모드 키 입력 시 대화 출력 이벤트

---
+++ NpcProject/Assets/Developers/Jinu/Scripts/TutorialKeyCheck.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class TutorialKeyCheck : MonoBehaviour
+{
+    [SerializeField]
+    private KEY_TYPE key;
+    [SerializeField]
+    private UnityEvent talkEvent;
+
+    public void CheckKey()
+    {
+        StartCoroutine(Check(key));
+    }
+
+    IEnumerator Check(KEY_TYPE key)
+    {
+        while(true)
+        {
+            if (Input.GetKeyDown(Managers.Game.Key.ReturnKey(key)))
+            {
+                talkEvent?.Invoke();
+                yield break;
+            }
+            yield return null;
+        }
+    }
+}
