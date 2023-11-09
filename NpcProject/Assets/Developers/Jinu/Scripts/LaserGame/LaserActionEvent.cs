32f8ee7051e0ed70bec8815b4622c54b59e838e5
solar <solar9134@gmail.com>
2023-11-10 05:17:34 +0900

    수정: 퍼즐 13, 신전 종료 이벤트 / 추가: 골렘 스피커

--- NpcProject/Assets/Developers/Jinu/Scripts/LaserGame/LaserActionEvent.cs
+++ NpcProject/Assets/Developers/Jinu/Scripts/LaserGame/LaserActionEvent.cs
@@ -7,13 +7,31 @@ public class LaserActionEvent : LaserAction
 {
     [SerializeField]
     private UnityEvent laserEvent;
+    [SerializeField]
+    private List<GameObject> laserObject;
 
     public override void StartLaserEvent()
     {
-        laserEvent?.Invoke();
+        if(CheckLaserActive())
+        {
+            laserEvent?.Invoke();
+        }        
     }
 
     public override void StopLaserEvent()
     {
+    }
+
+    private bool CheckLaserActive()
+    {
+        for(int i = 0; i < laserObject.Count; i++)
+        {
+            if(laserObject[i].activeSelf == false)
+            {
+                Debug.Log(laserObject[i].name + i + "is not Active");
+                return false;
+            }
+        }
+        return true;
     }
 }
